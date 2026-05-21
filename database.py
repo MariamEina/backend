35943fb43f8a6b21a89840d831202e37c2467461
Mariam Beruashvili <133596996+mariamberuashvili@users.noreply.github.com>
2026-05-21 13:46:21 +0200

    clean

--- database.py
+++ database.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -16,23 +15,4 @@ SessionLocal = sessionmaker(
     bind=engine
 )
 
-=======
-from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
-
-DATABASE_URL: str = "sqlite:///./app.db"
-
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
-
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
-
->>>>>>> 8daf375c1bac7d03115267e4ba65a66ef7d53413
 Base = declarative_base()
--- models.py
+++ models.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from sqlalchemy import String
 
 from sqlalchemy.orm import Mapped
@@ -24,31 +23,4 @@ class UserPhoto(Base):
 
     photo: Mapped[str] = mapped_column(
         String(255)
-=======
-from sqlalchemy import String
-
-from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
-
-from database import Base
-
-
-class UserPhoto(Base):
-    __tablename__ = "user_photos"
-
-    id: Mapped[int] = mapped_column(
-        primary_key=True
-    )
-
-    nia: Mapped[str] = mapped_column(
-        String(50)
-    )
-
-    dni: Mapped[str] = mapped_column(
-        String(50)
-    )
-
-    photo: Mapped[str] = mapped_column(
-        String(255)
->>>>>>> 8daf375c1bac7d03115267e4ba65a66ef7d53413
     )
